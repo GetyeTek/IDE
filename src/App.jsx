e6460e33fc45b8bdbe13c4d4fe70f4fd4db6370a
GetyeTek <getyeteklu2@gmail.com>
2025-12-25 12:43:03 +0300

    Conduit: 1 ops

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import Home from './views/Home.jsx';
+import Discover from './views/Discover.jsx';
+import Study from './views/Study.jsx';
+import Connect from './views/Connect.jsx';
+import Profile from './views/Profile.jsx';
