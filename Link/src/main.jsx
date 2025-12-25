0c8265138d7e24c3b9500205d2988f4f3e3d5c67
GetyeTek <getyeteklu2@gmail.com>
2025-12-25 12:25:24 +0300

    Conduit: 1 ops

--- Link/src/main.jsx
+++ Link/src/main.jsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+console.log("Main.jsx loaded. Attempting to mount React app...");
+
+const rootElement = document.getElementById('root');
+if (!rootElement) console.error("FATAL: Could not find #root element!");
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
