e0ecac45ea8ff3c5d031c14ddae0389c3a73fa6c
GetyeTek <getyeteklu2@gmail.com>
2026-01-21 01:03:28 +0300

    Conduit: 1 ops

--- IDE3/tests/broken.java
+++ IDE3/tests/broken.java
@@ -1,6 +1,6 @@
 public class BrokenJava {
     public static void main(String[] args) {
-        int x = 5 // Error: Missing semicolon
+        int x = 5; // Error: Missing semicolon
         System.out.println(x);
     }
 }
